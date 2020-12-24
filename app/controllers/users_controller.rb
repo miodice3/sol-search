6ccbceaacfad6854cafc359a01ea6d90b35680b6
Michael Iodice <miodice3@gmail.com>
2020-12-24 08:38:52 -0700

    excluded loggin before to fix sign up error since validations yesterday

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
+    before_action :require_login, except: [:create]
 
     def create #creating new user
         @user = User.new(user_params)
             if @user.save
                 session[:user_id] = @user.id
-                redirect_to success_path
+                redirect_to root_path
             else
                 render 'sessions/signup'
             end
