558f4220c2bea7a5a4e0f31986b4253cc484c8a2
Michael Iodice <miodice3@gmail.com>
2021-01-07 20:31:12 -0700

    fixed missing lines in all three config files to enable use of URL helpers instead of path helpers to redirect back to site from external source

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # config.action_mailer.preview_path = "#{Rails.root}/spec/mailers/previews"
-
+  config.action_mailer.default_url_options = { :host => "http://localhost:3000" }
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
